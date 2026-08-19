79e6c886ce54fd48f90482ff870ce238219d49c8
xblspain-glitch <xblspain@gmail.com>
2026-08-19 08:48:28 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const DATA='./';
-const APP_VERSION='3.1.63';
+const APP_VERSION='3.1.65';
+document.getElementById('appVersionNumber')?.replaceChildren(APP_VERSION);
 const CACHE_PREFIX='biblia-estudio-';
 const DICTIONARY_EQUIVALENCE_CHOICES_KEY='biblia_dictionary_equivalence_choices_v3150';
 const BOOK_READING_HISTORY_KEY_V3153='biblia_book_reading_history_v3153';
--- bible-app-loader.js
+++ bible-app-loader.js
@@ -1,4 +1,4 @@
-const version='3.1.63';
+const version='3.1.65';
 
 function paintStoredReadingPointV3163(){
   try{
