25060d8fd4ad1c1dd59d50090cba12656af73c23
xblspain-glitch <xblspain@gmail.com>
2026-09-16 18:03:22 +0200

    Add files via upload

--- bible-app-loader-safe-v3119.js
+++ bible-app-loader-safe-v3119.js
@@ -1,4 +1,4 @@
-const version=window.BIBLE_APP_VERSION_V1||'3.1.147';
+const version=window.BIBLE_APP_VERSION_V1||'3.1.148';
 const startupScriptsV3119=['app.js?v='+version,'progress-map.js?v='+version];
 
 function paintStoredReadingPointV3163(){
--- bible-app-loader.js
+++ bible-app-loader.js
@@ -1,4 +1,4 @@
-const version='3.1.147';
+const version='3.1.148';
 
 function paintStoredReadingPointV3163(){
   try{
